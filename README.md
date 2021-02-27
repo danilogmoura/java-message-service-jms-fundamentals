15dc08ad57d0936100381953da24ca8b0624d18b
Danilo <danilogmoura@gmail.com>
2021-02-26 23:44:13 -0300

    P2P in action

--- README.md
+++ README.md
@@ -37,7 +37,7 @@ Course offered by [Bharath Thippireddy](http://www.bharaththippireddy.com/).
 - [x] Configure JNDI properties
 - [x] Write a message to the Queue
 - [x] Consume a message from the Queue
-- [ ] P2P in action
+- [x] P2P in action
 - [ ] Summary So Far
 - [ ] Publish Subscribe in action
 - [ ] Use QueueBrowser
--- jmsfundamentals/src/main/java/com/dgm/jms/basics/FirstQueue.java
+++ jmsfundamentals/src/main/java/com/dgm/jms/basics/FirstQueue.java
@@ -9,15 +9,16 @@ public class FirstQueue {
     public static void main(String[] args) {
 
         InitialContext initialContext = null;
+        Connection connection = null;
 
         try {
             initialContext = new InitialContext();
             ConnectionFactory factory = (ConnectionFactory) initialContext.lookup("ConnectionFactory");
-            Connection connection = factory.createConnection();
+            connection = factory.createConnection();
 
             Session session = connection.createSession();
 
-            Queue queue = (Queue) initialContext.lookup("queue/myQueue=");
+            Queue queue = (Queue) initialContext.lookup("queue/myQueue");
             MessageProducer producer = session.createProducer(queue);
 
             TextMessage message = session.createTextMessage("I am the creator of my destiny");
@@ -35,7 +36,23 @@ public class FirstQueue {
 
         } catch (NamingException | JMSException e) {
             e.printStackTrace();
-        }
+        } finally {
+            if (initialContext != null) {
+                try {
+                    initialContext.close();
+                } catch (NamingException e) {
+                    e.printStackTrace();
+                }
+            }
 
+            if(connection != null ){
+                try {
+                    connection.close();
+                } catch (JMSException e) {
+                    e.printStackTrace();
+                }
+            }
     }
+
+}
 }
