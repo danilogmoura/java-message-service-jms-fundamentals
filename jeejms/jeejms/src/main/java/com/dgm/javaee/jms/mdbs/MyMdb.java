a7e0e4c3071c0b5e301b80c86b1a964691aebff4
Danilo <danilogmoura@gmail.com>
2021-03-01 18:57:12 -0300

    Deploy and Run

--- jeejms/jeejms/src/main/java/com/dgm/javaee/jms/mdbs/MyMdb.java
+++ jeejms/jeejms/src/main/java/com/dgm/javaee/jms/mdbs/MyMdb.java
@@ -1,11 +1,18 @@
 package com.dgm.javaee.jms.mdbs;
 
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 import java.util.logging.Logger;
 
+@MessageDriven(name = "MyMdb", activationConfig = {
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/myQueue"),
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+        @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+})
 public class MyMdb implements MessageListener {
 
     private static Logger LOGGER = Logger.getLogger(MyMdb.class.toString());
